3fb12ae241cd6fb4864430af4c2f48b2d89591a8
Nexabot1234 <bigdhanz1@gmail.com>
2026-05-28 05:50:44 +0100

    Update api/index.js with serverless-http export

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
-module.exports = (req, res) => {
+const serverless = require('serverless-http');
+
+module.exports = serverless(async (req, res) => {
   res.status(200).json({ status: 'ok', brand: 'NexaBot', message: 'API is live' });
-};
+});
